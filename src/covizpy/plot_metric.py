700e929c270518868b810ef4fbf5b97ae43fe476
Rohit Rawat <rohitrawat26@gmail.com>
2022-01-14 16:20:27 -0800

    added docstring to func

---
+++ src/covizpy/plot_metric.py
@@ -0,0 +1,27 @@
+def plot_metric(
+    country=["Canada"],
+    metric="positive_rate",
+    date_from=None,
+    date_to=None
+):
+    """
+    Create a line chart presenting specific country/countries COVID total new
+    cases and another metric for a time period
+    
+    Parameters
+    ----------
+    country : list, optional
+        A list of the country names of interests, by default ["Canada"]
+    val : str, optional
+        The name of the metric to be plotted with the new COVID cases. 
+        It can be one of the these: "reproduction_rate", "positive_rate", "cardiovasc_death_rate ",
+        or any other numeric column
+    date_from : str, optional
+        Start date of the plot, by default "None" will use 2 weeks prior today's date
+    date_to : str, optional
+        End date of the plot, by default "None" will use today's date
+    Returns
+    -------
+    chart
+        The line chart created
+    """
