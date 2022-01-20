5e81b60812d52f47f157f9cbcbb78dd0dc3a9a1c
Ting Zhe Yan <1884616+ytz@users.noreply.github.com>
2022-01-19 16:07:21 -0800

    Added code for plot_summary

--- src/covizpy/plot_summary.py
+++ src/covizpy/plot_summary.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import altair as alt
+from dateutil.parser import parse
+
+
 def plot_summary(
     var="location",
     val="new_cases",
@@ -28,3 +33,37 @@ def plot_summary(
     top_n : int, optional
         Specify number of qualitative values to show, by default 5
     """
+    # Load data
+    df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+
+    # Exception Handling
+    if not isinstance(df, pd.DataFrame):
+        raise FileNotFoundError("Data not found! There may be a problem with data URL.")
+
+    if df[var].dtypes.kind != "O":
+        raise TypeError("var needs to be a categorical variable!")
+
+    if df[val].dtypes.kind == "O":
+        raise TypeError("val needs to be a numeric variable!")
+
+    if not isinstance(top_n, int) or top_n <= 0:
+        raise ValueError("top_n must be an integer bigger than zero")
+
+    # Parse date, else raise ValueError
+    date_from = parse(date_from)
+    date_to = parse(date_to)
+
+    # Convert 'date' to date format
+    df["date"] = pd.to_datetime(df["date"])
+
+    # Filter by date
+    df = df.query("date >= @date_from & date <= @date_to")
+
+    # Remove aggregated locations
+    df = df[~df["iso_code"].str.startswith("OWID")]
+
+    # Aggregation
+    df_plot = df.groupby(var).agg({val: fun})[val].nlargest(top_n)
+    df_plot = df_plot.to_frame().reset_index()
+
+    return alt.Chart(df_plot).mark_bar().encode(y=alt.Y(var, sort="x"), x=val)
